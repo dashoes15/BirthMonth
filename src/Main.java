ce80b90bc9f83bf93d10d05e7d4dd629c5de3c31
dashoes15 <daltonasali@gmail.com>
2023-09-27 09:29:11 -0400

    lab 5 task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int birthMonth;
+        String invBirthMonthString;
+        double invBirthMonthDouble;
+        System.out.println("Enter your birth month:");
+        if (scan.hasNextInt()) {
+            birthMonth = scan.nextInt();
+            if (birthMonth > 0 && birthMonth < 13) {
+                System.out.println("Your birth month is " + birthMonth);
+            }else{
+                System.out.println("You have entered a invalid input of " + birthMonth);
+            }
+        } else if (scan.hasNext()) {
+            invBirthMonthString = scan.nextLine();
+            System.out.println("You have entered a invalid input of " + invBirthMonthString);
+        } else if (scan.hasNextDouble()){
+            invBirthMonthDouble = scan.nextDouble();
+            System.out.println("You have entered a invalid input of " + invBirthMonthDouble);
+        } else {
+        }
     }
 }
